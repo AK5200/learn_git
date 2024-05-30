ca04251587a5b6a39e55fbd9f11a7bac72f210b5
AK5200 <anupamkmr5200@gmail.com>
2024-05-30 12:53:04 +0530

    learning git

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+var arr = [1,2,3,4,"hey", true, ()=>{}];
+
+// //console.log(arr);
+
+// arr.forEach((val)=>{
+// console.log(val);
+// })
+
+// var newarr = arr.map((val)=>{
+//     return val+1;
+// })
+
+// console.log(newarr);
+    
+    
+// var ans = arr.find((val)=>{
+//     if(val<10) return val;
+// })
+
+// console.log(ans);
+
+//console.log(arr.indexOf(3));
+
+
+var fun = (a,b,c)=>{
+    console.log("hey");
+};
